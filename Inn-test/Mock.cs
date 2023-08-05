e4ddde7de71ef185cd073b09ca84fff05614c06b
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-08-05 23:51:41 +0300

    finished tests and swagger, might be buggy

--- Inn-test/Mock.cs
+++ Inn-test/Mock.cs
@@ -1,35 +1,130 @@
 ﻿using Async_Inn.Data;
 using Async_Inn.Models.Interfaces;
+using Async_Inn.Models.Services;
+using Async_Inn.Models;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Inn_test
+namespace AsyncInnTest
 {
     public abstract class Mock : IDisposable
     {
         private readonly SqliteConnection _connection;
+
+
         protected readonly AsyncInnDBContext _db;
-        protected readonly IAminity _am;
+        protected readonly IRoom _room;
+
         public Mock()
         {
             _connection = new SqliteConnection("Filename=:memory:");
             _connection.Open();
 
-            _db = new AsyncInnDBContext(new DbContextOptionsBuilder<AsyncInnDBContext>().UseSqlite(_connection).Options);
+            _db = new AsyncInnDBContext(
+                new DbContextOptionsBuilder<AsyncInnDBContext>()
+                .UseSqlite(_connection).Options);
+
 
             _db.Database.EnsureCreated();
+
+            _room = new RoomService(_db);
         }
+
+        protected async Task<Amenity> CreateAndSaveAmenity()
+        {
+            Amenity amenity = new Amenity()
+            {
+                Name = "Test"
+            };
+            _db.Amenities.Add(amenity);
+            await _db.SaveChangesAsync();
+
+            return amenity;
+        }
+
+        protected async Task<Amenity> CreateAndSaveAmenity2()
+        {
+            Amenity amenity = new Amenity()
+            {
+                Name = "Test1"
+            };
+            _db.Amenities.Add(amenity);
+            await _db.SaveChangesAsync();
+
+            return amenity;
+        }
+
+        protected async Task<Room> CreateAndSaveRoom()
+        {
+            Room room = new Room()
+            {
+                Name = "Test",
+                Layout = 1
+            };
+            _db.Rooms.Add(room);
+            await _db.SaveChangesAsync();
+            return room;
+        }
+        protected async Task<Room> CreateAndSaveRoom2()
+        {
+            Room room = new Room()
+            {
+                Name = "Test1",
+                Layout = 2
+            };
+            _db.Rooms.Add(room);
+            await _db.SaveChangesAsync();
+            return room;
+        }
+
+        protected async Task<Hotel> CreateAndSaveHotel()
+        {
+            Hotel hotel = new Hotel()
+            {
+                Name = "Test",
+                City = "city",
+                Country = "testC",
+                State = "tt",
+                StreetAddress = "st",
+                Phone = "0000"
+            };
+
+            _db.Hotels.Add(hotel);
+
+            await _db.SaveChangesAsync();
+
+            return hotel;
+
+        }
+        protected async Task<Hotel> CreateAndSaveHotel2()
+        {
+            Hotel hotel = new Hotel()
+            {
+                Name = "Asyncin",
+                City = "Amman",
+                Country = "Jordan",
+                State = "Jr",
+                StreetAddress = "st amman ",
+                Phone = "0785785578"
+            };
+
+            _db.Hotels.Add(hotel);
+
+            await _db.SaveChangesAsync();
+
+            return hotel;
+
+        }
+
+
+
+
 
         public void Dispose()
         {
             _db?.Dispose();
-            _connection.Dispose();
 
+            _connection?.Dispose();
         }
     }
 }
--- Inn-test/UnitTest1.cs
+++ Inn-test/UnitTest1.cs
@@ -1,10 +1,183 @@
-namespace Inn_test
+using Async_Inn.Models.Services;
+using Async_Inn.Models;
+using AsyncInnTest;
+
+namespace AsyncInnTest
 {
-    public class UnitTest1
+    public class UnitTest1 : Mock
     {
+
+        // room and amenity
         [Fact]
-        public void Test1()
+        public async void AddAmenityToRoom()
         {
+            var room = await CreateAndSaveRoom();
+
+            var amenity = await CreateAndSaveAmenity();
+
+            var roomAmenities = new RoomService(_db);
+
+            await roomAmenities.AddAmenityToRoom(room.ID, amenity.ID);
+
+            var result = await roomAmenities.GetRoom(room.ID);
+
+            Assert.Contains(result.Amenities, x => x.ID == amenity.ID);
+        }
+
+        [Fact]
+        public async void RemoveAmenityFromRoom()
+        {
+            var room = await CreateAndSaveRoom();
+
+            var amenity = await CreateAndSaveAmenity();
+
+            var roomAmenities = new RoomService(_db);
+
+            await roomAmenities.AddAmenityToRoom(room.ID, amenity.ID);
+
+            await roomAmenities.RemoveAmenityFromoRoom(room.ID, amenity.ID);
+
+            var result = await roomAmenities.GetRoom(room.ID);
+
+            Assert.DoesNotContain(result.Amenities, x => x.ID == amenity.ID);
+        }
+
+        [Fact]
+        public async Task HotelService_Should_Add_Hotel_Room()
+        {
+            // Arrange
+            var room = await CreateAndSaveRoom();
+            var hotel = await CreateAndSaveHotel();
+
+            var hotelRoom = new HotelRoomDTO()
+            {
+                HotelID = hotel.ID,
+                RoomNumber = 10,
+                Rate = 5.5M,
+                PetFriendly = true,
+                RoomID = room.ID
+            };
+
+            var hotelRoomService = new HotelRoomService(_db, _room);
+
+            // Act
+            await hotelRoomService.Create(hotelRoom, hotel.ID);
+
+            var hotelService = new HotelService(_db);
+
+
+            // Assert
+            var result = await hotelService.GetHotel(hotel.ID);
+            Assert.Contains(result.HotelRooms, x => x.RoomID == room.ID && x.HotelID == hotel.ID);
+        }
+
+        [Fact]
+        public async void UpdateANewHotelRoom()
+        {
+
+            var room = await CreateAndSaveRoom();
+
+            var hotel = await CreateAndSaveHotel();
+
+            var hotelRoom = new HotelRoomDTO()
+            {
+                HotelID = hotel.ID,
+                RoomNumber = 10,
+                Rate = 5.5M,
+                PetFriendly = true,
+                RoomID = room.ID
+            };
+
+            var hotelRoomService = new HotelRoomService(_db, _room);
+
+            await hotelRoomService.Create(hotelRoom, hotel.ID);
+
+            var hotelService = new HotelService(_db);
+
+
+            var result = await hotelService.GetHotel(hotel.ID);
+
+            Assert.Contains(result.HotelRooms, x => x.RoomID == room.ID);
+
+            Assert.Contains(result.HotelRooms, x => x.HotelID == hotel.ID);
+
+            var room2 = await CreateAndSaveRoom2();
+
+            var newHotelRoom = new HotelRoomDTO()
+            {
+                HotelID = hotel.ID,
+                RoomNumber = 10,
+                Rate = 9.5M,
+                PetFriendly = false,
+                RoomID = room2.ID
+            };
+
+            await hotelRoomService.UpdateHotelRoom(hotel.ID, newHotelRoom.RoomNumber, newHotelRoom);
+
+            var result2 = await hotelService.GetHotel(hotel.ID);
+
+            Assert.Contains(result2.HotelRooms, x => x.HotelID == hotel.ID);
+
+            Assert.Contains(result2.HotelRooms, x => x.RoomID == room2.ID);
+
+            Assert.DoesNotContain(result2.HotelRooms, x => x.RoomID == room.ID);
+        }
+
+        [Fact]
+        public async void RemoveHotelRooms()
+        {
+            var room = await CreateAndSaveRoom();
+
+            var hotel = await CreateAndSaveHotel();
+
+            var hotelRoom = new HotelRoomDTO()
+            {
+                HotelID = hotel.ID,
+                RoomNumber = 10,
+                Rate = 5.5M,
+                PetFriendly = true,
+                RoomID = room.ID
+            };
+
+            var hotelRoomService = new HotelRoomService(_db, _room);
+
+            await hotelRoomService.Create(hotelRoom, hotel.ID);
+
+            var hotelService = new HotelService(_db);
+
+
+            var result = await hotelService.GetHotel(hotel.ID);
+
+            Assert.Contains(result.HotelRooms, x => x.RoomID == room.ID);
+
+            Assert.Contains(result.HotelRooms, x => x.HotelID == hotel.ID);
+
+            await hotelRoomService.Delete(hotel.ID, hotelRoom.RoomNumber);
+            result = await hotelService.GetHotel(hotel.ID);
+            Assert.DoesNotContain(result.HotelRooms, x => x.RoomID == room.ID);
+
+            Assert.DoesNotContain(result.HotelRooms, x => x.HotelID == hotel.ID);
+        }
+
+        [Fact]
+        public async void AddHotelAndRemoveIt()
+        {
+            var hotel = await CreateAndSaveHotel();
+            var hotel2 = await CreateAndSaveHotel2();
+
+            var hotelService = new HotelService(_db);
+
+            var result = await hotelService.GetHotels();
+
+            Assert.Contains(result, x => x.ID == hotel.ID);
+            Assert.Contains(result, x => x.ID == hotel2.ID);
+
+            await hotelService.Delete(hotel.ID);
+
+            result = await hotelService.GetHotels();
+
+            Assert.DoesNotContain(result, x => x.ID == hotel.ID);
+            Assert.Contains(result, x => x.ID == hotel2.ID);
 
         }
     }
